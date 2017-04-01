f81cb537db3f52e6e87f6f093bc0eb621a7f3cee
Glenn Butcher <glenn.butcher@gmail.com>
2017-04-01 13:48:58 -0500

    add: ExposureCompensation

--- gimage.cpp
+++ gimage.cpp
@@ -1265,6 +1265,28 @@ void gImage::ApplySaturate(double saturate, int threadcount)
 }
 
 
+//Exposure Compensation
+//
+//Multiplies each R, G, and B value of each pixel by the multplier spedified in ev.
+//It should be noted that GUI sliders values cannot be applied directly; they must be
+//transformed into the delta from the last application in order to keep track of the 
+//total compensation applied.
+//
+
+void ApplyExposureCompensation(double ev, int threadcount=0)
+{
+	#pragma omp parallel for num_threads(threadcount)
+	for (unsigned x=0; x<w; x++) {
+		for (unsigned y=0; y<h; y++) {
+			unsigned pos = x + y*w;
+			image[pos].r *= ev;
+			image[pos].g *= ev;
+			image[pos].b *= ev;
+		}
+	}
+}
+
+
 
 //Tint
 //
