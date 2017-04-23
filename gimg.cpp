d64070c26b74baee017dd4f0baa86340e9ae411d
Glenn Butcher <glenn.butcher@gmail.com>
2017-04-23 12:54:59 -0500

    auto now blows by clipped highlights for white threshold

--- gimg.cpp
+++ gimg.cpp
@@ -259,9 +259,17 @@ for (int f=0; f<files.size(); f++)
 				std::vector<long> hdata = dib.Histogram();
 				long hmax=0;
 				for (i=0; i<256; i++) if (hdata[i] > hmax) hmax = hdata[i];
+				
+				//find the lower threshold, the black point
 				for (i=1; i<128; i++) if ((double)hdata[i]/(double)hmax > 0.05) break;
 				blk = (double) i;
-				for (i=255; i>=128; i--) if ((double)hdata[i]/(double)hmax > 0.05) break;
+				
+				//find the local max in 250-255:
+				int m = 0, l=255;
+				for (i=255; i>=250; i--) if (hdata[i]>m) {m = hdata[i]; l = i;}
+
+				//start looking for the white point threshold right after the location of the local max:
+				for (i=l-1; i>=128; i--) if ((double)hdata[i]/(double)hmax > 0.05) break;
 				wht = (double) i;
 			}
 			else {
